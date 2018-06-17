76373d4d968ec537e3ec753f10e91408f77ba9f0
DmLvkvch <31544742+DmLvkvch@users.noreply.github.com>
2018-06-17 15:15:25 +0300

    Add files via upload

---
+++ universal_array/count_if.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define N 20
+/*
+int compare_char(const void a){
+
+}
+*/
+int compare_int(const void * a) {
+    if((*(const int *)a )%2!=0 && (*(const int *)a )%3==0)
+      return 0;
+    else
+    return -1;
+}
+
+int compare_double(const void * a) {
+  double val=(*(const double*)a);
+  int intval=val;
+  if((val-intval)==0)
+    return compare_int(&intval);
+  return 0;
+}
+
+size_t universalMax(const void * base, size_t num, size_t size, int(*cmp)(const void *)) {
+    const char * pbase = (const char *)base;
+    size_t counter = 0;
+    int i;
+    for (i = 0; i < num; ++i)
+        if (cmp(pbase + i * size) == 0) 
+            counter++;
+    return counter;
+}
+
+int main() {
+    size_t size;
+    scanf("%zd", &size);
+
+    int (*compare)(const void *);
+    void * array;
+    char * format_string;
+    switch (size) {
+    case 4:
+        array = malloc(N * sizeof(int));
+        format_string = " %d";
+        compare = compare_int;
+        break;
+    case 8:
+        array = malloc(N * sizeof(double));
+        format_string = " %lf";
+        compare = compare_double;
+        break;
+    }
+    for (int i = 0; i < N; ++i)
+        scanf(format_string, array + i * size);
+    printf("%zd", universalMax(array, N, size, compare));
+    return 0;
+}
---
+++ universal_array/universalSum.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define N 20
+
+int compare_char(const void* a){
+    return *(const char*)a;
+}
+
+int compare_int(const void * a) {
+    return *(const int*)a;
+}
+
+int compare_double(const void * a) {
+  return *(const double*)a;
+
+}
+
+size_t universalMax(const void * base, size_t num, size_t size, int(*cmp)(const void *)) {
+    const char * pbase = (const char *)base;
+    int i;
+    int sum=0;
+    for (i = 0; i < num; ++i)
+        sum+=cmp(pbase+i*size);
+    return sum;
+}
+
+int main() {
+    size_t size;
+    scanf("%zd", &size);
+
+    int (*compare)(const void *);
+    void * array;
+    char * format_string;
+    switch (size) {
+    case 1:
+        array = malloc(N * sizeof(char));
+        format_string = " %c";
+        compare = compare_char;
+        break;
+    case 4:
+        array = malloc(N * sizeof(int));
+        format_string = " %d";
+        compare = compare_int;
+        break;
+    case 8:
+        array = malloc(N * sizeof(double));
+        format_string = " %lf";
+        compare = compare_double;
+        break;
+    }
+    for (int i = 0; i < N; ++i)
+        scanf(format_string, array + i * size);
+    printf("%zd", universalMax(array, N, size, compare));
+    return 0;
+}
