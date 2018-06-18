e706e2776e62a90f94fee51002ccd39e94b0c785
DmLvkvch <31544742+DmLvkvch@users.noreply.github.com>
2018-06-19 01:39:45 +0300

    Add files via upload

--- string/reverse_words.c
+++ string/reverse_words.c
@@ -7,20 +7,22 @@ int main(){
 	char words[100][100];
 	int counter = 0;
 	fgets(str, 100, stdin);
+	str[strlen(str)-1]='\0';
 	char* word=strtok(str, " ");
 	while(word!=NULL){
 		strcpy(*(words+counter), word);
 		word=strtok(NULL, " ");
 		counter++;
 	}
+	//words[counter-1][strlen(wo)]
 	for(int i=0;i<counter;i++){
-		for(int j=strlen(words[i])-1;j>=0;j--){
+		for(int j=strlen(words[i]);j>=0;j--){
 			if(!isdigit(words[i][j])){
-				if(strlen(words[i]-1) == j){
+				if(strlen(words[i])== j){
 					if(isalpha(words[i][j]))
-						printf("%c ",toupper(words[i][j]));
+						printf(" ");
 					else
-					printf("%c ",toupper(words[i][j]));
+						printf(" ");
 				}
 				else{
 					if(isalpha(words[i][j]))
