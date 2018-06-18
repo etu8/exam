697251560fe28bed7515f81e097a8bb60e5073e6
laskovenko1 <laskovenko1@yandex.ru>
2018-06-19 02:44:05 +0300

    Mod

--- string/reverse_words.c
+++ string/reverse_words.c
@@ -3,35 +3,40 @@
 #include <ctype.h>
 
 int main(){
-	char str[100];
-	char words[100][100];
-	int counter = 0;
-	fgets(str, 100, stdin);
-	str[strlen(str)-1]='\0';
-	char* word=strtok(str, " ");
-	while(word!=NULL){
-		strcpy(*(words+counter), word);
-		word=strtok(NULL, " ");
-		counter++;
-	}
-	//words[counter-1][strlen(wo)]
-	for(int i=0;i<counter;i++){
-		for(int j=strlen(words[i]);j>=0;j--){
-			if(!isdigit(words[i][j])){
-				if(strlen(words[i])== j){
-					if(isalpha(words[i][j]))
-						printf(" ");
-					else
-						printf(" ");
-				}
-				else{
-					if(isalpha(words[i][j]))
-						printf("%c",toupper(words[i][j]));
-					else
-						printf("%c",toupper(words[i][j]));
-				}
-			}
-		}
-	}	
-	return 0;
+    char str[100];
+    char words[100][100];
+    int counter = 0;
+    fgets(str, 100, stdin);
+    str[strlen(str)-1]='\0';
+    char* word=strtok(str, " ");
+    while(word!=NULL){
+        strcpy(*(words+counter), word);
+        word=strtok(NULL, " ");
+        counter++;
+    }
+    //words[counter-1][strlen(wo)]
+    for(int i=strlen(words[0]) - 1; i>=0; i--)
+    {
+        if(!isdigit(words[0][i]))
+            printf("%c", toupper(words[0][i]));
+    }
+    for(int i=1;i<counter;i++){
+        for(int j=strlen(words[i]);j>=0;j--){
+            if(!isdigit(words[i][j])){
+                if(strlen(words[i])== j){
+                    if(isalpha(words[i][j]))
+                        printf(" ");
+                    else
+                        printf(" ");
+                }
+                else{
+                    if(isalpha(words[i][j]))
+                        printf("%c",toupper(words[i][j]));
+                    else
+                        printf("%c",toupper(words[i][j]));
+                }
+            }
+        }
+    }
+    return 0;
 }
